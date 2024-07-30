0c8577453d3fd85d0d49022a4d6c9e9f26b9a1f7
ShaikSyfuddeen <syfuddeen107@gmail.com>
2024-07-31 00:01:19 +0530

    Controlling backpressure with subscription classes

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,21 +1,44 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import org.reactivestreams.Subscription;
+
+import reactor.core.publisher.BaseSubscriber;
 
 public class Exercise5 {
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
+        // Use ReactiveSources.intNumberFlux() and ReactiveSources.userMono()
 
         // Subscribe to a flux using the error and completion hooks
         // TODO: Write code here
+        // ReactiveSources.intNumbersFlux().subscribe(
+        // e -> System.out.println(e),
+        // err -> System.out.println(err.getMessage()),
+        // () -> System.out.println("Completed")
+        // );
 
         // Subscribe to a flux using an implementation of BaseSubscriber
         // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(new MySubscriber<>());
 
         System.out.println("Press a key to end");
         System.in.read();
+    }
+
+}
+
+class MySubscriber<T> extends BaseSubscriber<T> {
+
+    protected void hookOnSubscribe(Subscription subscription) {
+        System.out.println("Subscribe happened");
+        request(1);
+    }
+
+    public void hookOnNext(T value) {
+        System.out.println(value.toString() + " received");
+        request(1);
     }
 
 }
