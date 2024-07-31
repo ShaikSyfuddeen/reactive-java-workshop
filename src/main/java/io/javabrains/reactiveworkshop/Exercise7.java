9571417975e390371c2c569915a7b936cd0e9bec
ShaikSyfuddeen <syfuddeen107@gmail.com>
2024-07-31 23:04:53 +0530

    Using operators on Flux

--- src/main/java/io/javabrains/reactiveworkshop/Exercise7.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise7.java
@@ -11,24 +11,31 @@ public class Exercise7 {
 
         // Print all values from intNumbersFlux that's greater than 5
         // TODO: Write code here
+        // ReactiveSources.intNumbersFlux().filter(n -> n>5).log().subscribe(System.out::println);
 
         // Print 10 times each value from intNumbersFlux that's greater than 5
         // TODO: Write code here
+        // ReactiveSources.intNumbersFlux().log().filter(e -> e > 5).map(e -> e*10).subscribe(System.out::println);
 
         // Print 10 times each value from intNumbersFlux for the first 3 numbers emitted that's greater than 5
         // TODO: Write code here
+        // ReactiveSources.intNumbersFlux().filter(e -> e>5).map(e -> e*10).take(3).subscribe(System.out::println);
 
         // Print each value from intNumbersFlux that's greater than 20. Print -1 if no elements are found
         // TODO: Write code here
+        // ReactiveSources.intNumbersFlux().filter(e -> e>2).defaultIfEmpty(-1).subscribe(System.out::println);
 
         // Switch ints from intNumbersFlux to the right user from userFlux
         // TODO: Write code here
+        // ReactiveSources.intNumbersFlux().flatMap(i -> ReactiveSources.userFlux().filter(user -> user.getId() == i)).subscribe(System.out::println);
 
         // Print only distinct numbers from intNumbersFluxWithRepeat
         // TODO: Write code here
+        // ReactiveSources.intNumbersFluxWithRepeat().distinct().log().subscribe();
 
         // Print from intNumbersFluxWithRepeat excluding immediately repeating numbers
         // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithRepeat().distinctUntilChanged().log().subscribe();
 
         System.out.println("Press a key to end");
         System.in.read();
