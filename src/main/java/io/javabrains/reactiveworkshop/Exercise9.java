9d8f7a82f405d0e86adf6f9dbbc61752bdec9202
ShaikSyfuddeen <syfuddeen107@gmail.com>
2024-07-31 23:39:41 +0530

    Flux to Mono

--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -11,12 +11,15 @@ public class Exercise9 {
 
         // Print size of intNumbersFlux after the last item returns
         // TODO: Write code here
+        // ReactiveSources.intNumbersFlux().count().log().subscribe();
 
         // Collect all items of intNumbersFlux into a single list and print it
         // TODO: Write code here
+        // ReactiveSources.intNumbersFlux().collectList().log().subscribe();
 
         // Transform to a sequence of sums of adjacent two numbers
         // TODO: Write code here
+        ReactiveSources.intNumbersFlux().buffer(2).map(list -> list.get(0) + list.get(1)).log().subscribe();
 
         System.out.println("Press a key to end");
         System.in.read();
